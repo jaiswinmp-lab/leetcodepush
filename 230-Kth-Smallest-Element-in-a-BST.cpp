8cb81dc4c080c229c1545bff348d941c15c5b9c3
JAISWIN M P <jaiswinmp@gmail.com>
2026-09-22 14:40:43 +0530

    [230] [Time Beats: 100.00%] [Memory Beats: 92.18%] - LeetPush

---
+++ 230-Kth-Smallest-Element-in-a-BST.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int count = 0;
+    int result = 0;
+
+    void inorder(TreeNode* root, int k) {
+        if(root == NULL) {
+            return;
+        }
+
+        inorder(root->left, k);
+
+        count++;
+
+        if(count == k) {
+            result = root->val;
+            return;
+        }
+
+        inorder(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return result;
+    }
+};
